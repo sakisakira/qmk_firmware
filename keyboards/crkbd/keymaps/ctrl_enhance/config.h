ef557682451de5f36cc51d4bce5d53d75ad49c0c
SAkira a.k.a. Akira Suzuki <sakisakira@gmail.com>
2020-11-10 20:09:30 +0900

    [Keymap] Fix timing of tapping.  Set pressing TAB to ctrl.

--- keyboards/crkbd/keymaps/ctrl_enhance/config.h
+++ keyboards/crkbd/keymaps/ctrl_enhance/config.h
@@ -33,10 +33,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 250
+#define TAPPING_TERM 150
 
 #define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
